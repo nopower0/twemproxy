ef2d45f7d0528f876054dc2097262196d501ec88
lizhe <lizhe@bytedance.com>
2014-05-07 11:11:44 +0800

    add release abtest

--- build.sh
+++ build.sh
@@ -10,10 +10,3 @@ make
 echo 
 echo "make finished"
 
-output_bin=../twemproxy_bin/bin
-if [[ -d $output_bin && -w $output_bin ]]; then
-    cp -rp ./src/nutcracker $output_bin/twemproxy
-    echo "cp bin finished"
-else
-    echo "$output_bin not exists or access denied"
-fi
---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bin_file=twemproxy
+if [[ ! -z "$1" && "$1" == "abtest" ]]; then
+    bin_file=twemproxy_abtest
+fi
+output_dir=../twemproxy_bin/bin
+if [[ -d $output_dir && -w $output_dir ]]; then
+    cp -rvp ./src/nutcracker $output_dir/$bin_file
+    echo "cp bin finished"
+else
+    echo "$output_dir not exists or access denied"
+fi
